3e613556db70842cdb4e39a99d8bad4789238f7f
Marin Leontenko <marin.leontenko@gmail.com>
2019-04-09 10:16:08 +0200

    osnovni redux setup

---
+++ src/store/reducers/places.js
@@ -0,0 +1,13 @@
+const initalState = {
+    places: [],
+    selectedPlace: null
+};
+
+const reducer= (state = initialState, action) => {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+};
+
+export default reducer;
